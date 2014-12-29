b27cc0ecff839a8e412355f237740ee90005de8d
kaaterskil <blair@kaaterskil.com>
2014-12-29 08:22:42 -0500

    Edit Thymeleaf configuration

--- src/main/java/laxstats/Application.java
+++ src/main/java/laxstats/Application.java
@@ -1,18 +1,13 @@
 package laxstats;
 
-import nz.net.ultraq.thymeleaf.LayoutDialect;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
-import org.thymeleaf.spring4.SpringTemplateEngine;
 
-@SuppressWarnings("unused")
 @Configuration
 @EnableJpaRepositories
 @Import(RepositoryRestMvcConfiguration.class)
--- src/main/java/laxstats/ThymeleafConfiguration.java
+++ src/main/java/laxstats/ThymeleafConfiguration.java
@@ -3,12 +3,15 @@ package laxstats;
 import nz.net.ultraq.thymeleaf.LayoutDialect;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.thymeleaf.extras.springsecurity3.dialect.SpringSecurityDialect;
 import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
 import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
-@Configuration
+/**
+ * Note: We don't seem to need to autoload this file.
+ */
+// @Configuration
 public class ThymeleafConfiguration {
 
 	@Bean
@@ -16,9 +19,9 @@ public class ThymeleafConfiguration {
 		final ServletContextTemplateResolver resolver = new ServletContextTemplateResolver();
 		resolver.setPrefix("classpath:/templates/");
 		resolver.setSuffix(".html");
+		resolver.setCharacterEncoding("UTF-8");
 		resolver.setTemplateMode("HTML5");
 		resolver.setOrder(1);
-		resolver.setCharacterEncoding("UTF-8");
 		resolver.setCacheable(false);
 		return resolver;
 	}
@@ -26,9 +29,16 @@ public class ThymeleafConfiguration {
 	@Bean
 	public SpringTemplateEngine templateEngine() {
 		final SpringTemplateEngine templateEngine = new SpringTemplateEngine();
-		templateEngine.addTemplateResolver(templateResolver());
+		templateEngine.setTemplateResolver(templateResolver());
 		templateEngine.addDialect(new SpringSecurityDialect());
 		templateEngine.addDialect(new LayoutDialect());
 		return templateEngine;
+	}
+
+	@Bean
+	public ThymeleafViewResolver thymeleafViewResolver() {
+		final ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+		resolver.setTemplateEngine(templateEngine());
+		return resolver;
 	}
 }
