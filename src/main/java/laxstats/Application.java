0a4e7b3cab5dfb29689382f6b0a66a1dfb24df94
kaaterskil <blair@kaaterskil.com>
2015-07-12 05:59:41 -0400

    Enable yaml property configuration

--- src/main/java/laxstats/Application.java
+++ src/main/java/laxstats/Application.java
@@ -2,10 +2,12 @@ package laxstats;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 // Use @SpringBootApplication: same as using the combined
 // @Configuration, @EnableAutoConfiguration and @ComponentScan annotations
 @SpringBootApplication
+@EnableConfigurationProperties
 public class Application {
 
    public static void main(String[] args) {
