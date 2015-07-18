1305fa33252c2545bac16b688580cd2b14daa4b7
kaaterskil <blair@kaaterskil.com>
2015-07-18 16:18:13 -0400

    Fix incomplete query in team query repository

--- src/main/java/laxstats/query/teams/TeamQueryRepository.java
+++ src/main/java/laxstats/query/teams/TeamQueryRepository.java
@@ -7,19 +7,17 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(collectionResourceRel = "teams", path = "teams")
-public interface TeamQueryRepository extends
-		PagingAndSortingRepository<TeamEntry, String> {
+@RepositoryRestResource
+public interface TeamQueryRepository extends PagingAndSortingRepository<TeamEntry, String> {
 
-	@Query("select count(*) from TeamEntry te "
-			+ "where upper(te.sponsor) = upper(?1) "
-			+ "and upper(te.name) = upper(?2) "
-			+ "and te.gender = ?3 and te.letter = ?4")
-	int uniqueTeam(String sponsor, String name, TeamGender gender, Letter letter);
+   @Query("select count(*) from TeamEntry te "
+      + "where upper(te.sponsor) = upper(cast(?1 as text)) "
+      + "and upper(te.name) = upper(cast(?2 as text)) and te.gender = ?3 and te.letter = ?4")
+   int uniqueTeam(String sponsor, String name, TeamGender gender, Letter letter);
 
-	@Query("select count(*) from TeamEntry te "
-			+ "where te.sponsor is not null and upper(te.sponsor) = upper(?1) "
-			+ "and te.name is not null and upper(te.name) = upper(?2)")
-	int updateTeam(String sponsor, String name, TeamGender gender,
-			Letter letter, String id);
+   @Query("select count(*) from TeamEntry te "
+      + "where te.sponsor is not null and upper(te.sponsor) = upper(cast(?1 as text)) "
+      + "and te.name is not null and upper(te.name) = upper(cast(?2 as text)) "
+      + "and te.gender = ?3 and te.letter = ?4 and te.id <> ?5")
+   int updateTeam(String sponsor, String name, TeamGender gender, Letter letter, String id);
 }
